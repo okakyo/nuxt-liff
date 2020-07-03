626f6ac719b0c3daa022d8b2ddfbbd6253ff22f2
OkaKyo <0622okakyo@gmail.com>
2020-07-03 18:15:37 +0900

    アプリを設計途中

---
+++ components/templates/forms/header/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar>
+    <v-btn>Top</v-btn>
+    <v-spacer />
+    <v-btn to="/login">ログイン</v-btn>
+  </v-app-bar>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card>
+    <v-btn>LINEでログイン</v-btn>
+    <v-btn>Googleでログイン</v-btn>
+    <v-btn>メールでログイン</v-btn>
+  </v-card>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
