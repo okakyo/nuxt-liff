73e9c33b0aa3916a4fc105a345ffac9712dc0cb0
OkaKyo <0622okakyo@gmail.com>
2020-07-07 14:07:11 +0900

    LIFF 設計中

--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -13,28 +13,35 @@
 </template>
 <script>
 import liff from '@line/liff'
-import { defineComponent, ref } from '@vue/composition-api'
-export default defineComponent({
-  async setup() {
-    // MEMO: created が廃止されて setup()
-    await liff.init({
-      liffId: '1640064557-P25n2Nd9',
-    })
-    const profile = await liff.getProfile()
-    const userId = ref()
-    const displayName = ref('')
-    const pictureUrl = ref('https://bulma.io/images/placeholders/128x128.png')
-    const statusMessage = ref('')
-    userId.value = profile.userId
-    displayName.value = profile.displayName
+export default {
+  data() {
     return {
-      userId,
-      displayName,
-      pictureUrl,
-      statusMessage,
+      userId: '',
+      displayName: '',
+      pictureUrl: 'https://bulma.io/images/placeholders/128x128.png',
+      statusMessage: '',
     }
   },
-})
+  created() {
+    liff
+      .init({
+        liffId: '1640064557-P25n2Nd9',
+      })
+      .then(() => {
+        // ここで認証機能を実装したい
+        console.log('LIFF is Ready!')
+        liff.getProfile().then((profile) => {
+          this.userId = profile.userId
+          this.displayName = profile.displayName
+          this.pictureUrl = profile.pictureUrl
+          this.statusMessage = profile.statusMessage
+        })
+      })
+      .catch((e) => {
+        console.error(e)
+      })
+  },
+}
 </script>
 
 <style>
